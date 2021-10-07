ab166ae0d5cab7e9a9be707bc83d1f7d8bd7adaf
juanlu90 <juanlu.calderon@gmail.com>
2021-10-07 20:52:33 +0200

    movie search functionality added

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,25 +1,53 @@
-import './Filters.css';
+import "./Filters.css";
+import { SetStateAction } from "react";
 
-interface Filters {
+interface IFilters {
   info: {
-    page: number,
-    results: Array<{}>,
-    total_pages: number,
-    total_results: number
-  },
-  onChangePage: any
-};
+    page: number;
+    results: Array<{}>;
+    total_pages: number;
+    total_results: number;
+  };
+  showSearch?: boolean;
+  setSearchMovie?: React.Dispatch<SetStateAction<string>>;
+  searchMovie?: string;
+  onChangePage: (page: number) => void;
+  onChangeSearch?: any;
+}
 
-const Filters: React.FC<Filters> = ({info, onChangePage}) => {
-
+const Filters: React.FC<IFilters> = (props) => {
+  const { info, onChangePage, onChangeSearch, showSearch } = props;
   return (
     <div className="filters">
       <div>
-       <button onClick={() => onChangePage(info.page - 1)} disabled={info.page <= 1}> Prev </button>
-       <span>{info?.page} of {info?.total_pages}</span>
-       <button onClick={() => onChangePage(info.page + 1)} disabled={info.page >= info.total_pages}> Next </button>
-      </div>  
-      <span>Items: {20 * info.page} of {info.total_results}</span>    
+        <button
+          onClick={() => onChangePage(info.page - 1)}
+          disabled={info.page <= 1}
+        >
+          Prev
+        </button>
+        <span>
+          {info?.page} of {info?.total_pages}
+        </span>
+        <button
+          onClick={() => onChangePage(info.page + 1)}
+          disabled={info.page >= info.total_pages}
+        >
+          Next
+        </button>
+      </div>
+      {showSearch && (
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => onChangeSearch(e)}
+          placeholder="search"
+        />
+      )}
+      <span>
+        Items: {20 * info.page} of {info.total_results}
+      </span>
     </div>
   );
 };
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,71 +1,113 @@
-import { useEffect, useState } from 'react';
-import Card from '../Card/Card';
-import Filters from '../Filters/Filters';
-import MovieDetails from '../MovieDetails/MovieDetails';
-import './MoviesList.css';
+import { useEffect, useState } from "react";
+import Card from "../Card/Card";
+import Filters from "../Filters/Filters";
+import MovieDetails from "../MovieDetails/MovieDetails";
+import { SetStateAction } from "react";
+import "./MoviesList.css";
 
 interface Main {
-    info: {
-        page: number,
-        results: Array<{}>,
-        total_pages: number,
-        total_results: number
-    };
-    setInfo: any
-};
+  info: {
+    page: number;
+    results: Array<{}>;
+    total_pages: number;
+    total_results: number;
+  };
+  setInfo: React.Dispatch<SetStateAction<number>>;
+}
 
 interface movieDetails {
-    isShow: boolean,
-    movieId: number,
-    movieSelected: any
+  isShow: boolean;
+  movieId: number;
+  movieSelected: any;
 }
 
-const MoviesList: React.FC<Main> = ({info, setInfo}) => {
-const initialMovieDetails = {
+const MoviesList: React.FC<Main> = ({ info, setInfo }) => {
+  const initialMovieDetails = {
     isShow: false,
     movieId: 0,
-    movieSelected: {}
-}
+    movieSelected: {},
+  };
 
-const [showMovieDetails, setShowMovieDetails] = useState<movieDetails>(initialMovieDetails);
+  const [showMovieDetails, setShowMovieDetails] =
+    useState<movieDetails>(initialMovieDetails);
 
-let apiUrl = `https://api.themoviedb.org/3/movie/${showMovieDetails.movieId}`;
-    let apiKey = '?api_key=f1eb0fddb91524fcb1e67860576e0d66&';
+  const [searchMovie, setSearchMovie] = useState<string>("");
 
-    const fetchUrl = `${apiUrl}${apiKey}&language=en-US`;
+  let apiUrl = `https://api.themoviedb.org/3/movie/${showMovieDetails.movieId}`;
+  let apiKey = "?api_key=f1eb0fddb91524fcb1e67860576e0d66&";
 
-    const fetchMovieDetails = async () => {
-        try {
-            const response = await fetch(fetchUrl);
-            const json = await response.json();
-            setShowMovieDetails(prevState => ({...prevState, isShow: true, movieSelected: json}));
-        } catch (error) {
-            console.log("error", error);
+  const fetchUrl = `${apiUrl}${apiKey}&language=en-US`;
+
+  const fetchMovieDetails = async () => {
+    try {
+      const response = await fetch(fetchUrl);
+      const json = await response.json();
+      setShowMovieDetails((prevState) => ({
+        ...prevState,
+        isShow: true,
+        movieSelected: json,
+      }));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const fetchSearchMovie = async (title: string) => {
+    console.log(title);
+    const fetchSearchMovie = `https://api.themoviedb.org/3/search/movie?api_key=f1eb0fddb91524fcb1e67860576e0d66&query=${title}`;
+    try {
+      const response = await fetch(fetchSearchMovie);
+      const json = await response.json();
+      console.log(json);
+      setInfo(json);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const onChangePage = (value: number) => {
+    setInfo((prevState: any) => ({ ...prevState, page: value }));
+  };
+
+  const onChangeSearch = (e: any) => {
+    fetchSearchMovie(e.target.value);
+  };
+
+  useEffect(() => {
+    if (showMovieDetails.movieId !== 0) fetchMovieDetails();
+  }, [showMovieDetails.movieId]);
+
+  return (
+    <div className="moviesList">
+      <h1>List of movies</h1>
+      <Filters
+        info={info}
+        onChangePage={onChangePage}
+        showSearch={true}
+        setSearchMovie={setSearchMovie}
+        searchMovie={searchMovie}
+        onChangeSearch={onChangeSearch}
+      />
+      <div
+        className={
+          showMovieDetails.isShow ? "movieListMovieSelected" : undefined
         }
-    };
-
-    const onChangePage = (value: number) => {
-        setInfo((prevState: any) => ({...prevState, page: value}))
-    };
-
-    useEffect(() => {
-        if(showMovieDetails.movieId !== 0) fetchMovieDetails();
-    }, [showMovieDetails.movieId]);
-
-    return (
-        <div className="moviesList">
-            <h1>List of movies</h1>
-            <Filters info={info} onChangePage={onChangePage} />
-            <div className={showMovieDetails.isShow ? 'movieListMovieSelected' : undefined}>
-                <div className="moviesListCards">
-                    {info.results.map((x: any, i: number) => 
-                        <Card movie={x} key={i} setShowMovieDetails={setShowMovieDetails} />
-                    )}
-                </div>
-                {showMovieDetails.isShow && <MovieDetails setShowMovieDetails={setShowMovieDetails} movieSelected={showMovieDetails.movieSelected} /> }
-            </div>
-            <Filters info={info} onChangePage={onChangePage} />
+      >
+        <div className="moviesListCards">
+          {info.results?.map((x: any, i: number) => (
+            <Card movie={x} key={i} setShowMovieDetails={setShowMovieDetails} />
+          ))}
+          {!info.results?.length && <span>No results</span>}
         </div>
+        {showMovieDetails.isShow && (
+          <MovieDetails
+            setShowMovieDetails={setShowMovieDetails}
+            movieSelected={showMovieDetails.movieSelected}
+          />
+        )}
+      </div>
+      <Filters info={info} onChangePage={onChangePage} />
+    </div>
   );
 };
 
